6a2dfdf8868fc3797aa7c1b147d76e756953c026
ADMIN <ADMIN@CCLPC>
2026-06-15 02:06:10 +0900

    添加项目文件。

---
+++ IndustrialMonitor/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using IndustrialMonitor.Modules.Dashboard;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace IndustrialMonitor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<MainWindow,MainWindowViewModel>();
+
+            
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<Dashboard>();
+            base.ConfigureModuleCatalog(moduleCatalog);
+        }
+
+    }
+
+}
---
+++ IndustrialMonitor/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndustrialMonitor
+{
+    internal class MainWindowViewModel:BindableBase
+    {
+        private readonly IRegionManager _RegionManager;
+
+        public DelegateCommand<string> NavigateCommand { get; }
+
+
+
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+            _RegionManager = regionManager;
+
+            NavigateCommand = new DelegateCommand<string>((region) =>
+            {
+                regionManager.Regions["ContentRegion"].RequestNavigate(region);
+            });
+        }
+
+    }
+}
