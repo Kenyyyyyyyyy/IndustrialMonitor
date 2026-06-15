4ea29b3d50e39cd1fa3212f3153512be479665fe
56409 <564090962@qq.com>
2026-06-15 16:38:02 +0900

    6.15 16:37

--- IndustrialMonitor.Modules.Dashboard/DashboardViewModel.cs
+++ IndustrialMonitor.Modules.Dashboard/DashboardViewModel.cs
@@ -13,13 +13,41 @@ namespace IndustrialMonitor.Modules.Dashboard
             TodayOutput = 1250;
         }
 
-        private int _todayoutput;
+        #region allview
+        private int _todayOutPut;
 
         public int TodayOutput
         {
-            get { return _todayoutput; }
-            set { SetProperty(ref _todayoutput,value); }
+            get => _todayOutPut; 
+            set => SetProperty(ref _todayOutPut, value); 
         }
 
+        private int _yieldRate;
+
+        public int YieldRate
+        {
+            get => _yieldRate;
+            set => SetProperty(ref _yieldRate, value);
+        }
+
+        private int _alarmCount;
+
+        public int AlarmCount
+        {
+            get => _alarmCount;
+            set => SetProperty(ref _alarmCount, value);
+        }
+
+        private int _deviceStatus;
+
+        public int DeviceStatus
+        {
+            get => _deviceStatus;
+            set => SetProperty(ref _deviceStatus, value);
+        }
+
+
+
+        #endregion
     }
 }
